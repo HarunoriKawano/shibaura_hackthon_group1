d574d71e2d0af6a2beef6a20d7b69c9d05b29b22
MotokiHirose <75714401+Motoki-Rose@users.noreply.github.com>
2022-04-07 13:52:12 +0900

    edit views

--- no_more_ramen/ramen_record/views.py
+++ no_more_ramen/ramen_record/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.shortcuts import render
 from rest_framework import viewsets,filters
 
@@ -6,9 +7,11 @@ from .serializer import RamenRecordSerializer
 # Create your views here.
 
 class RamenRecordViewSet(viewsets.ModelViewSet):
-    queryset = RamenRecord.objects.all()
+    queryset = RamenRecord.objects.filter(user.)
     serializer_class = RamenRecordSerializer
     def get_parameters(self,request):
+        return response
     def create_data(self,request):
+        ramencategory = request.GET.get(category)
     def get_carender(self,request):
         
